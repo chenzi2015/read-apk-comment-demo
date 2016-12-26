96ccbb1f14a53e81545a64f0c13095349816ac62
chenzi <admin@chenzi.net>
2016-12-26 23:19:10 +0800

    Fixd get comment error

--- app/src/main/java/net/chenzi/readapkcommentdemo/MainActivity.java
+++ app/src/main/java/net/chenzi/readapkcommentdemo/MainActivity.java
@@ -1,14 +1,16 @@
 package net.chenzi.readapkcommentdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import net.chenzi.readapkcommentdemo.lib.ZipStream;
-
 import java.io.File;
 import java.io.RandomAccessFile;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private String mComment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +24,18 @@ public class MainActivity extends AppCompatActivity {
         //red apk comment
         try {
             File file = new File(path);
-            RandomAccessFile apkFile = new RandomAccessFile(file,"r");
+            RandomAccessFile apkFile = new RandomAccessFile(file, "r");
             ZipStream zipStream = new ZipStream(apkFile);
-            String commnet = zipStream.getComment();
+            mComment = zipStream.getComment();
 
-            System.out.println("zip comment = " + commnet);
+            System.out.println("zip comment = " + mComment);
         } catch (Exception e) {
             System.out.println(e.toString());
         }
 
+        if (!mComment.equals("")) {
+            TextView textView = (TextView)findViewById(R.id.textView);
+            textView.setText("ZIP注释内容：" + mComment);
+        }
     }
 }
--- app/src/main/java/net/chenzi/readapkcommentdemo/lib/ZipStream.java
+++ app/src/main/java/net/chenzi/readapkcommentdemo/lib/ZipStream.java
@@ -1,10 +1,6 @@
 package net.chenzi.readapkcommentdemo.lib;
 
-import java.io.ByteArrayInputStream;
-import java.io.DataInputStream;
-import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.util.Arrays;
 
 /**
  * Read Apk comment
@@ -12,26 +8,10 @@ import java.util.Arrays;
  */
 public class ZipStream {
 
-    /**
-     * zip end magic
-     */
-    private static long MAGIC = 0x06054b50;
-    /**
-     * zip end magic string
-     */
-    private static String StringEndArchive = "PK\005\006";
-    /**
-     * max comment length
-     */
-    private static long maxComment = 100;
-    /**
-     * zip file object
-     */
     private RandomAccessFile mZipFile;
-    /**
-     * zip comment
-     */
-    private String mComment;
+    private static final long LOCSIG = 0x4034b50, ENDSIG = 0x6054b50;
+    private static final int ENDHDR = 22;
+
 
     /**
      * ZipStream construct
@@ -42,79 +22,58 @@ public class ZipStream {
         mZipFile = file;
     }
 
+    /**
+     * Get zip comment
+     *
+     * @return String comment
+     * @throws Exception
+     */
     public String getComment() throws Exception {
-        long fileSize = mZipFile.length();
-        long startPosition = Math.max((fileSize - maxComment), 0);
-        System.out.println("filesize=" + fileSize);
-        System.out.println("maxComment=" + maxComment);
-        System.out.println("startPosition=" + startPosition);
-
-        //seek to start position
-        mZipFile.seek(startPosition);
-        int readLength = (int) (fileSize - startPosition);
-        System.out.println("readLength=" + readLength);
-
-        //read end data
-        byte[] endData = new byte[readLength];
-        mZipFile.read(endData, 0, readLength);
-        String endDataString = new String(endData, "UTF-8");
-        System.out.println("endData=" + endDataString);
-        System.out.println("endDataByte=" + Arrays.toString(endData));
-
-        //find the zip end magic string position
-        int StringEndArchivePosition = endDataString.indexOf(StringEndArchive);
-        System.out.println("StringEndArchivePosition=" + StringEndArchivePosition);
-        if (StringEndArchivePosition == -1) {
-            return "";
+        long scanOffset = mZipFile.length() - ENDHDR;
+        if (scanOffset < 0) {
+            throw new Exception("File too short to be a zip file: " + mZipFile.length());
         }
 
-        //check sign
-        long endRecordPosition = startPosition + StringEndArchivePosition;
-        System.out.println("StringEndArchivelength=" + StringEndArchive.length());
-        mZipFile.seek(endRecordPosition);
-        System.out.println("StringEndArchivelengthReadLine=" + mZipFile.readLine());
+        mZipFile.seek(0);
+        final int headerMagic = Integer.reverseBytes(mZipFile.readInt());
+        if (headerMagic == ENDSIG) {
+            throw new Exception("Empty zip archive not supported");
+        }
+        if (headerMagic != LOCSIG) {
+            throw new Exception("Not a zip archive");
+        }
 
-        byte[] sign = new byte[4];
-        mZipFile.read(sign);
-        String signString = new String(sign, "UTF-8");
-        System.out.println("getFilePointer=" + mZipFile.getFilePointer());
-        System.out.println("signString=" + signString);
-        System.out.println("signByte=" + Arrays.toString(sign));
-        System.out.println("signStringlength=" + signString.length());
+        long stopOffset = scanOffset - 65536;
+        if (stopOffset < 0) {
+            stopOffset = 0;
+        }
 
-        //read comment length
-        mZipFile.seek(endRecordPosition + 20);
+        while (true) {
+            mZipFile.seek(scanOffset);
+            if (Integer.reverseBytes(mZipFile.readInt()) == ENDSIG) {
+                break;
+            }
+
+            scanOffset--;
+            if (scanOffset < stopOffset) {
+                throw new Exception("End Of Central Directory signature not found");
+            }
+        }
+
+        mZipFile.seek(mZipFile.getFilePointer() + ENDHDR - 6);
         byte[] commentLengthByte = new byte[2];
         mZipFile.read(commentLengthByte);
-
         int commentLength = byte2int(commentLengthByte);
-        System.out.println("getFilePointer=" + mZipFile.getFilePointer());
-        System.out.println("commentLength=" + Arrays.toString(commentLengthByte));
-        System.out.println("commentLength=" + commentLength);
 
-        //read comment
-        mZipFile.seek(endRecordPosition + 22);
-        byte[] commentByte = new byte[commentLength];
-        mZipFile.read(commentByte);
-        System.out.println("commentByte=" + Arrays.toString(commentByte));
-        mComment = new String(commentByte, "UTF-8");
+        if (commentLength > 0) {
+            byte[] commentBytes = new byte[commentLength];
+            mZipFile.read(commentBytes);
+            String comment = new String(commentBytes);
 
-        //close
-        mZipFile.close();
+            return comment.trim();
+        }
 
-        return mComment.trim();
-    }
-
-
-    /**
-     * Check signature
-     */
-    private boolean isSignature(String sign) {
-        String ss = String.valueOf((int) MAGIC);
-        System.out.println("isSignature=" + ss + "|end");
-
-
-        return true;
+        return "";
     }
 
     /**
